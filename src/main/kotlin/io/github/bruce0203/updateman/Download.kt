917fc05dfa791d48f70fafbf2d73f75f4b26171b
Bruce0203 <BruceFreedy@gmail.com>
2022-12-20 00:39:49 +0900

    fix: download

--- src/main/kotlin/io/github/bruce0203/updateman/Download.kt
+++ src/main/kotlin/io/github/bruce0203/updateman/Download.kt
@@ -15,6 +15,6 @@ fun download(pluginName: String, url: String, destiny: String) {
         if (pl !== null) destinyFile else File("plugins/${destinyFile.name}")
     )
     println("Downloaded  ${destinyFile.name}")
-    if (pl == null) PluginUtil.load(pl)
+    if (pl == null) PluginUtil.load(destinyFile.name)
     else PluginUtil.reload(pl)
 }
